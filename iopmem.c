48d20c8b492e3ee72b98cbaee90f7bbe1333c559
Logan Gunthorpe <logang@deltatee.com>
2016-10-13 14:02:22 -0600

    Change copyright.

--- iopmem.c
+++ iopmem.c
@@ -1,6 +1,6 @@
 /*
  * IOPMEM Block Device Driver
- * Copyright (c) 2016, PMC-Sierra, Inc.
+ * Copyright (c) 2016, Microsemi Corporation
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
